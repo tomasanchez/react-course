f4356372f6fb4080a3eb2c1021eb510374d15498
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-15 14:16:26 -0300

    fix(component): label display

--- myapp/src/components/ObjectPage/ObjectAttribute.jsx
+++ myapp/src/components/ObjectPage/ObjectAttribute.jsx
@@ -13,14 +13,26 @@ function ObjectAttribute({
   stateValue,
   state,
   direction = FlexBoxDirection.Row,
+  tooltip,
 }) {
   return (
-    <FlexBox direction={direction} style={{ marginLeft: "1rem" }}>
-      <Label wrap={true} showColon={true} style={{ marginRight: "0.5rem" }}>
-        {label}
-      </Label>
+    <FlexBox
+      direction={direction}
+      style={label && { marginLeft: "1rem" }}
+      tooltip={tooltip}
+    >
+      {label && (
+        <Label
+          wrap={true}
+          showColon={true}
+          style={{ marginRight: label && "0.5rem" }}
+        >
+          {label}
+        </Label>
+      )}
       <ObjectStatus
         icon={icon && <Icon name={icon} />}
+        tooltip={tooltip}
         state={
           state
             ? state
