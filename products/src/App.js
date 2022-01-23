4d779d6ebd707facf4d9bad7ce1febf2bcbc6938
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-23 13:15:43 -0300

    refactor: usign auth context

--- products/src/App.js
+++ products/src/App.js
@@ -6,19 +6,22 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./layout/Object/Product";
 import SignUp from "./layout/FullScreen/SignUp";
 import SignIn from "./layout/FullScreen/SignIn";
+import AuthProvider from "./auth/Auth";
 
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/products" />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/products" element={<Worklist />}></Route>
-          <Route path="/products/:id" element={<Product />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/products" />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/products" element={<Worklist />}></Route>
+            <Route path="/products/:id" element={<Product />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
