9a1caa3d79759385463619ee9fe5e739e4e0b732
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-23 01:00:59 -0300

    refactor: sign in

--- products/src/App.js
+++ products/src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import { ThemeProvider } from "@ui5/webcomponents-react";
 import "./App.css";
 import Worklist from "./layout/Worklist/Worklist";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LogIn from "./layout/FullScreen/LogIn";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./layout/Object/Product";
 import SignUp from "./layout/FullScreen/SignUp";
+import SignIn from "./layout/FullScreen/SignIn";
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LogIn />} />
+          <Route path="/" element={<Navigate to="/products" />} />
+          <Route path="/login" element={<SignIn />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/products" element={<Worklist />}></Route>
           <Route path="/products/:id" element={<Product />} />
